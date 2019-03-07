9689dfa99258225480ce7dbd67d450cee165a9a7
jdaza13 <jdaza1327@gmail.com>
2019-03-07 09:00:31 +0000

    cambios campos motores

--- app/transformer/transformer.component.js
+++ app/transformer/transformer.component.js
@@ -29,13 +29,16 @@ angular.
           $scope.transformadores.push({
             id: i,
             tipo: 3,
-            voltaje: null,
+            voltajeprimario: null,
+            voltajesecundario: null,
             potenicaparente: null,
-            potenciareactiva: null,
-            potenciaactiva: null,
-            corriente: null,
-            factordepotencia: null,
-            caballosdefuerza: null,
+            potenciareactivasecundaria: null,
+            potenciareactivaprimaria: null,
+            potenciaactivasecundaria: null,
+            potenciaactivaprimaria: null,
+            corrienteprimaria: null,
+            corrientesecundaria: null,
+            factordepotencia: 0.9,            
             correccion: true,
             conexionesExternas: [],
             numeromotores: 0,
@@ -46,25 +49,23 @@ angular.
       };
       
       $scope.updatemotores = function(transformadorId) {
-        $scope.transformadores[transformadorId-1].motores=[];
-        $scope.motoresIds = [];
+        $scope.transformadores[transformadorId-1].motores=[];        
         for(var i = 1; i <= $scope.transformadores[transformadorId-1].numeromotores; i++){
           $scope.transformadores[transformadorId-1].motores.push({
             id: i,
             tipo: 3,
-            voltaje: null,
-            potenicaparente: null,
+            voltaje: $scope.transformadores[transformadorId-1].voltajesecundario,
+            potenciaaparente: null,
             potenciareactiva: null,
             potenciaactiva: null,
             corriente: null,
             factordepotencia: null,
             caballosdefuerza: null,
-            correccion: true,
-            conexionesExternas: []
-          });
-          $scope.motoresIds.push(i);
+            correccion: true            
+          });          
         }
       }; 
+
       
       $scope.actualizarConexionesTransformador = function(transformadorId, transformadorExtId) {
         var indexLocal = $scope.transformadores[transformadorId-1].conexionesExternas.indexOf(transformadorExtId);
@@ -80,23 +81,35 @@ angular.
           $scope.transformadores[transformadorId-1].conexionesExternas.push(transformadorExtId);
           $scope.transformadores[transformadorExtId-1].conexionesExternas.push(transformadorId);
         }
+      };     
+    
+      $scope.onTransformadorVoltajeSecundarioChange = function(transformadorId) {
+        //potencia activa secundaria (factor de potencia ,)
+        //potencia reactiva secundaria 
+        //potencia aparente
+        //corriente secundaria 
+        //
       };
-      
-      $scope.actualizarConexionesMotor = function(transformadorId, motorId, motorExtId) {
-        var indexLocal = $scope.transformadores[transformadorId-1].motores[motorId-1].conexionesExternas.indexOf(motorExtId);
-        var indexExterno = $scope.transformadores[transformadorId-1].motores[motorExtId-1].conexionesExternas.indexOf(motorId);
+
+      $scope.onMotorFactordePontenciaChange = function(transformadorId, motorId){
+        var currentMotor = $scope.transformadores[transformadorId-1].motores[motorId-1];
+        //corriente (voltaje,potencia activa o potencia aparente o potencia reactiva)
+        if(currentMotor.potenciaaparente) {
+          switch(currentMotor.tipo){
+            case 1: currentMotor.corriente = currentMotor.potenciaaparente / currentMotor.voltaje* currentMotor.factordepotencia;
+              break;
+            case 2: currentMotor.corriente = currentMotor.potenciaaparente / currentMotor.voltaje* currentMotor.factordepotencia;
+              break;
+            case 3: currentMotor.corriente = currentMotor.potenciaaparente / currentMotor.voltaje* currentMotor.factordepotencia *Math.pow(3, 1/2);;
+              break;
+          }
+
+        }
         
-        // Remover la conexión
-        if (indexLocal > -1) {
-          $scope.transformadores[transformadorId-1].motores[motorId-1].conexionesExternas.splice(indexLocal, 1);
-          $scope.transformadores[transformadorId-1].motores[motorExtId-1].conexionesExternas.splice(indexExterno, 1);
-        }
-        // Agregar la conexión
-        else {
-          $scope.transformadores[transformadorId-1].motores[motorId-1].conexionesExternas.push(motorExtId);
-          $scope.transformadores[transformadorId-1].motores[motorExtId-1].conexionesExternas.push(motorId);
-        }
-      };
+        //potenciaaparente
+        //potencia reactiva 
+
+      }
 
       $scope.show=function(){
         console.log($scope.datosgenerador);
